c22f4a362ba11a05b2236994151e4f550869f2bb
Daniela-MV <danmv.0991@gmail.com>
2025-04-23 20:57:23 +0000

    ya hice la estructura del semáforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,10 +6,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            <div className="bg-dark," style={{with: "5px", height: "50px"}}>
-
-			</div>
+		<div className="d-flex flex-column align-items-center justify-content-center mt-5 ">
+            <div className="bg-dark" style={{width: "20px", height: "80px"}}></div>
+			<div className="bg-dark" style={{width: "150px", height: "300px"}}></div>
 		</div>
 	);
 };
