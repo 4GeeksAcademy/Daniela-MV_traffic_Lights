88d3b98f63f4f8ba7df6d7f09810a9dfda6b4c0b
Daniela-MV <danmv.0991@gmail.com>
2025-04-24 04:33:43 +0000

    Ya hice el color rojo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [colorRed, setColorRed]= useState("btn-danger opacity-50");
+
+	const colorRojo = () => {
+		if (colorRed === "btn-danger opacity-50"){
+		setColorRed("btn-danger");
+	} else{
+		setColorRed("btn-danger opacity-50");
+	}
+};
+	
+	
 	return (
 		<div className="d-flex flex-column align-items-center justify-content-center mt-5 ">
-            <div className="bg-dark" style={{width: "20px", height: "80px"}}></div>
-			<div className="bg-dark" style={{width: "150px", height: "300px"}}></div>
+			<div className="bg-dark" style={{ width: "30px", height: "90px" }}></div>
+			<div className="bg-dark" style={{ width: "150px", height: "450px" }}>
+				<div className="d-flex flex-column" aria-label="Basic mixed styles example">
+					<button 
+					type="button" 
+					className={`btn ${colorRed} mt-5 mx-4 rounded-circle`} 
+					style={{height: "80px"}} 
+					onClick={colorRojo}>
+					</button>
+					<button type="button" className="btn btn-warning mt-5 mx-4 rounded-circle opacity-50" style={{height: "80px"}}></button>
+					<button type="button" className="btn btn-success mt-5 mx-4 rounded-circle opacity-50" style={{height: "80px"}}></button>
+				</div>
+			</div>
 		</div>
 	);
 };
