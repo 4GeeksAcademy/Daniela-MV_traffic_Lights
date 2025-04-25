8eb7d36d7d4fdb78d8175ad8396443a6a6a38905
Daniela-MV <danmv.0991@gmail.com>
2025-04-25 01:41:33 +0000

    boton para aleatorizar

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,47 +6,60 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 
-	const [colorRed, setColorRed] = useState("btn-danger opacity-50");
-	// const [colorYellow, setColorYellow] = useState("btn-warning opacity-50");
-	// const [colorGreen, setColorGreen] = useState("btn-success opacity-50")
+	const [colorRed, setColorRed] = useState("btn-danger opacity-25");
+	const [colorYellow, setColorYellow] = useState("btn-warning opacity-25");
+	const [colorGreen, setColorGreen] = useState("btn-success opacity-25");
+	const [aleatorio, setAleatorio] = useState("colorRed")
 
 	const colorRojo = () => {
-		if (colorRed === "btn-danger opacity-50") {
+		if (colorRed === "btn-danger opacity-25") {
 			setColorRed("btn-danger");
 		} else {
-			setColorRed("btn-danger opacity-50");
+			setColorRed("btn-danger opacity-25");
 		}
 	};
 
-	// const colorAmarillo = () => {
-	// 	if (colorYellow === "btn-warning opacity-50") {
-	// 		setColorYellow("btn-warning");
-	// 	} else {
-	// 		setColorYellow("btn-warning opacity-50");
-	// 	}
-	// };
+	const colorAmarillo = () => {
+		if (colorYellow === "btn-warning opacity-25") {
+			setColorYellow("btn-warning");
+		} else {
+			setColorYellow("btn-warning opacity-25");
+		}
+	};
 
-	// const colorVerde = () => {
-	// 	if (colorGreen === "btn-success opacity-50") {
-	// 		setColorGreen("btn-success");
-	// 	} else {
-	// 		setColorGreen("btn-success opacity-50");
-	// 	}
-	// };
+	const colorVerde = () => {
+		if (colorGreen === "btn-success opacity-25") {
+			setColorGreen("btn-success");
+		} else {
+			setColorGreen("btn-success opacity-25");
+		}
+	};
+
+	function coloresAl(){
+	const aleatorizar = ["coloRed", "colorYellow", "colorGreen"];
+	const inicioAleatorio = Math.floor(Math.random()* aleatorizar.length);
+	const mostrarAleatorio = aleatorizar[inicioAleatorio];
+	console.log(mostrarAleatorio);
+    }
 
 
 	return (
-		<div className="d-flex flex-column align-items-center justify-content-center mt-5 ">
-			<div className="bg-dark" style={{ width: "30px", height: "90px" }}></div>
-			<div className="bg-dark" style={{ width: "150px", height: "450px" }}>
-				<div className="d-flex flex-column" aria-label="Basic mixed styles example">
-					<button type="button" className={`btn ${colorRed} mt-5 mx-4 rounded-circle`} style={{ height: "80px" }} onClick={colorRojo}>
-					</button>
-					{/* <button type="button" className={`btn ${colorYellow} mt-5 mx-4 rounded-circle`} style={{ height: "80px" }} onClick={colorAmarillo}>
-					</button>
-					<button type="button" className={`btn ${colorGreen} mt-5 mx-4 rounded-circle`} style={{ height: "80px" }} onClick={colorVerde}>
-					</button> */}
+		<div className="d-flex justify-content-evenly">
+			<div className="d-flex flex-column align-items-center justify-content-center mt-5">
+				<div className="bg-dark" style={{ width: "30px", height: "90px" }}></div>
+				<div className="bg-dark" style={{ width: "150px", height: "450px" }}>
+					<div className="d-flex flex-column" aria-label="Basic mixed styles example">
+						<button type="button" className={`btn ${colorRed} mt-5 mx-4 rounded-circle`} style={{ height: "80px" }} onClick={colorRojo}>
+						</button>
+						<button type="button" className={`btn ${colorYellow} mt-5 mx-4 rounded-circle`} style={{ height: "80px" }} onClick={colorAmarillo}>
+						</button>
+						<button type="button" className={`btn ${colorGreen} mt-5 mx-4 rounded-circle`} style={{ height: "80px" }} onClick={colorVerde}>
+						</button>
+					</div>
 				</div>
+			</div>
+			<div className="d-flex align-items-center">
+			<button type="button" className="btn btn-outline-info" onClick={coloresAl}>Cambia el color</button>
 			</div>
 		</div>
 	);
